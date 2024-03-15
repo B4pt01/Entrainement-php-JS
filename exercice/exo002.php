fdb7d98ffe0c5257edf557b7eaa9e4a346af8e4c
baptiste vauthier <ba.vauthier@gmail.com>
2024-03-15 13:44:20 +0100

    exo 2

--- exercice/exo002.php
+++ exercice/exo002.php
@@ -4,5 +4,34 @@ include("partials/menu.php");
 ?>
 
 <?php
+$nombre = rand(0, 20);
+
+if ($nombre < 7) {
+    echo "Bonjour";
+} elseif ($nombre >= 7 && $nombre < 14) {
+    echo "Salut";
+} else {
+    echo "Hello";
+}
+
+echo "<br> le nombre est : ", $nombre;
+?>
+<br>
+<br>
+<script>
+    var nombre = Math.floor(Math.random() * 21);
+
+    if (nombre < 7) {
+        document.write("Bonjour");
+    } else if (nombre >= 7 && nombre < 14) {
+        document.write("Salut");
+    } else {
+        document.write("Hello");
+    }
+    document.write(" <br> le nombre est: ", nombre);
+</script>
+
+
+<?php
 include("partials/footer.php");
 ?>
